08393d42b81a6289682c24a963b891e55b73c513
Siddhant <raosiddhant99@gmail.com>
2019-11-11 18:57:31 +0530

    Add mining capability

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use blockchainlib::*;
 
 fn main() {
-    let mut block = Block::new(0, now(), vec![0; 32], 0, "Genisis block!".to_owned());
+    let mut block = Block::new(0, now(), vec![0; 32], 0, 
+    "Genisis block!".to_owned(), 0x0000ffffffffffffffffffffffffffff);
     println!("{:?}", &block);
-
-    block.hash = block.hash();
+    
+    block.mine();
+    println!("{:?}", &block);
+    
+    block.mine();
     println!("{:?}", &block);
 }
